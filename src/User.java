966937d4d9d21dc7bb928ecdfbc08ffaed3ea7dc
lijieyu <lijieyu20031211@gmail.com>
2024-04-11 19:33:11 +0200

    User Class wordt gemaakt

---
+++ src/User.java
@@ -0,0 +1,35 @@
+public class User {
+    private String username;
+    private String password;
+    private int userId;
+
+    public User(String username, String password, int userId) {
+        this.username = username;
+        this.password = password;
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
